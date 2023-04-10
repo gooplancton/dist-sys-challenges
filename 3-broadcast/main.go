f1046750bdc9806658ff92c6d57b9babacd9048c
gooplancton <gooplancton@outlook.com>
2023-04-10 03:02:19 +0200

    implemented point 3b

--- 3-broadcast/main.go
+++ 3-broadcast/main.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+	"github.com/tidwall/gjson"
+	"golang.org/x/exp/slices"
+  "github.com/samber/lo"
 )
+
 
 func main() {
 	n := maelstrom.NewNode()
 	values := []float64{}
+  checkpoints := make(map[string]int)
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
-		msgBody := make(map[string]interface{})
+		var msgBody map[string]interface{}
+		json.Unmarshal(msg.Body, &msgBody)
+    topologyPath := fmt.Sprintf("topology.%s", n.ID())
+    neighbours := gjson.GetBytes(msg.Body, topologyPath).Array()
+    for _, neighbourId := range(neighbours) {
+      checkpoints[neighbourId.Str] = 0
+    }
 		msgBody["type"] = "topology_ok"
+		delete(msgBody, "topology")
 
 		return n.Reply(msg, msgBody)
 	})
@@ -23,12 +34,25 @@ func main() {
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var msgBody map[string]interface{}
 		json.Unmarshal(msg.Body, &msgBody)
-		value, ok := msgBody["message"].(float64)
-		if !ok {
-			return errors.New(fmt.Sprintf("invalid message field: %s", msgBody["message"]))
-		}
-
+    value := gjson.GetBytes(msg.Body, "message").Float()
+    _skipIds := gjson.GetBytes(msg.Body, "skip_ids").Array() 
+    var skipIds []string = lo.Map(_skipIds, func(r gjson.Result, _ int) string {
+      return r.Str
+    })
 		values = append(values, value)
+
+    for neighbourId := range checkpoints {
+      if slices.Contains(skipIds, neighbourId) {
+        continue
+      }
+      
+      broadcastBody := make(map[string]interface{})
+      broadcastBody["skip_ids"] = append(skipIds, n.ID())
+      broadcastBody["type"] = "broadcast"
+      broadcastBody["message"] = value
+      go n.Send(neighbourId, broadcastBody)
+    }
+    
 		delete(msgBody, "message")
 		msgBody["type"] = "broadcast_ok"
 
@@ -45,7 +69,12 @@ func main() {
 		return n.Reply(msg, msgBody)
 	})
 
+  n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
+    return nil
+  })
+
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
+
