bba3762332c1441068e7816f2b9c68025816c97d
gooplancton <gooplancton@outlook.com>
2023-04-11 04:07:08 +0200

    minor fixes

--- 3-broadcast/main.go
+++ 3-broadcast/main.go
@@ -13,35 +13,32 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-func BroadcastValue(n *maelstrom.Node, neighbourId string, value float64, skipIds []string) bool {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
+func BroadcastValue(n *maelstrom.Node, neighbourId string, value int64, skipIds []string) bool {
 	broadcastBody := make(map[string]interface{})
-	broadcastBody["skip_ids"] = append(skipIds, n.ID())
 	broadcastBody["type"] = "broadcast"
 	broadcastBody["message"] = value
-	msgReply, err := n.SyncRPC(ctx, neighbourId, broadcastBody)
+	msgReply, err := n.SyncRPC(context.Background(), neighbourId, broadcastBody)
 	msgReplyType := gjson.GetBytes(msgReply.Body, "type").Str
 
-	return err != nil && msgReplyType != "broadcast_ok"
+	return msgReplyType == "broadcast_ok" && err == nil
 }
 
 type ResendJob struct {
 	NeighbourId string
-	Value float64
-	SkipIds []string
+	Value       int64
+	SkipIds     []string
 }
 
 func main() {
 	n := maelstrom.NewNode()
-	values := []float64{}
+	values := []int64{}
 	neighbours := []string{}
-	resendChan := make(chan *ResendJob, 100)
+	resendChan := make(chan *ResendJob, 1)
 
 	go func() {
 		for resendJob := range resendChan {
-			if ok := BroadcastValue(n, resendJob.NeighbourId, resendJob.Value, resendJob.SkipIds); !ok {
+			ok := BroadcastValue(n, resendJob.NeighbourId, resendJob.Value, resendJob.SkipIds)
+			if !ok {
 				time.Sleep(1)
 				resendChan <- resendJob
 			}
@@ -53,9 +50,9 @@ func main() {
 		json.Unmarshal(msg.Body, &msgBody)
 		topologyPath := fmt.Sprintf("topology.%s", n.ID())
 		_neighbours := gjson.GetBytes(msg.Body, topologyPath).Array()
-		neighbours = lo.Map(_neighbours, func (res gjson.Result, _ int) string {
+		neighbours = lo.Map(_neighbours, func(res gjson.Result, _ int) string {
 			return res.Str
-		}) 
+		})
 		msgBody["type"] = "topology_ok"
 		delete(msgBody, "topology")
 
@@ -65,24 +62,25 @@ func main() {
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var msgBody map[string]interface{}
 		json.Unmarshal(msg.Body, &msgBody)
-		value := gjson.GetBytes(msg.Body, "message").Float()
+		value := gjson.GetBytes(msg.Body, "message").Int()
 		_skipIds := gjson.GetBytes(msg.Body, "skip_ids").Array()
 		var skipIds []string = lo.Map(_skipIds, func(r gjson.Result, _ int) string {
 			return r.Str
 		})
-		values = append(values, value)
 
-		for _, neighbourId := range neighbours {
-			if slices.Contains(skipIds, neighbourId) {
-				continue
-			}
+		if !slices.Contains(values, value) {
+			values = append(values, value)
 
-			go func(nodeId string) {
-				if ok := BroadcastValue(n, nodeId, value, skipIds); !ok {
-					time.Sleep(1)
-					resendChan <- &ResendJob{NeighbourId: nodeId, Value: value, SkipIds: skipIds}
+			for _, neighbourId := range neighbours {
+				if slices.Contains(skipIds, neighbourId) {
+					continue
 				}
-			}(neighbourId)
+
+				ok := BroadcastValue(n, neighbourId, value, append(skipIds, n.ID()))
+				if !ok {
+					resendChan <- &ResendJob{NeighbourId: neighbourId, Value: value, SkipIds: skipIds}
+				}
+			}
 		}
 
 		delete(msgBody, "message")
